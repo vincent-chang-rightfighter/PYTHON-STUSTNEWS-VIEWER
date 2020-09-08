ec9d4a59dcce24a4db93764dbf0a963001e443ce
vincent <9475656+vincent-chang-rightfighter@users.noreply.github.com>
2020-09-09 00:16:43 +0800

    Update stustnews.py
    V 0.0.5 code update

--- source/stustnews.py
+++ source/stustnews.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # coding: utf-8
 # %%
 from bs4 import BeautifulSoup
@@ -12,13 +13,16 @@ import tkinter
 import threading
 import webbrowser
 import emoji
+import textwrap
 from icon import img
 from tkinter.ttk import Treeview
-from tkinter import Tk, Scrollbar, Frame, ttk, messagebox
+from tkinter import Tk, Scrollbar, Frame, StringVar, PhotoImage, ttk, messagebox, font
 program_directory = sys.path[0]
+string_ = ""
 
 root = Tk()
-root.geometry('1095x700+400+200')
+style = ttk.Style()
+root.geometry('1100x800+100+100')
 root.resizable(False, False)
 root.title('南台消息查詢程式')
 tmp = open("tmp.ico", "wb+")
@@ -29,6 +33,14 @@ os.remove("tmp.ico")
 
 headers = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36'}
+
+fontset = font.Font(family="Microsoft JhengHei", size=11, weight="normal")
+fontset2 = font.Font(family="Microsoft JhengHei", size=12, weight="normal")
+LARGE_FONT = 12
+style.configure("Treeview.Heading", font=("Microsoft JhengHei", LARGE_FONT),
+                rowheight=int(LARGE_FONT*2.5))
+style.configure("Treeview", font=("Microsoft JhengHei", LARGE_FONT),
+                rowheight=int(LARGE_FONT*4.5))
 
 
 def exit_func():
@@ -43,10 +55,10 @@ def load_4page_btn_func():
     if tree.get_children():
         for i in tree.get_children():
             tree.delete(i)
-    single_page(1)
-    single_page(2)
-    single_page(3)
-    single_page(4)
+    single_page_crawler(1)
+    single_page_crawler(2)
+    single_page_crawler(3)
+    single_page_crawler(4)
 
 
 def load_4page_btn_func_thread():
@@ -56,7 +68,7 @@ def load_4page_btn_func_thread():
 
 def program_info_btn_func():
     MsgBox = tkinter.messagebox.askquestion(
-        "南台消息查詢程式", "程式版本 : V 0.0.4\n是否前往 GITHUB?", icon='warning')
+        "南台消息查詢程式", "程式版本 : V 0.0.5\n是否前往GITHUB?", icon='warning')
     if MsgBox == 'yes':
         webbrowser.open(
             "https://github.com/vincent-chang-rightfighter/PYTHON-STUSTNEWS-VIEWER")
@@ -75,7 +87,8 @@ def search_btn_func():
     if tree.get_children():
         for i in tree.get_children():
             tree.delete(i)
-    search_page(X)
+    search_page_crawler(X)
+    search_entrytext.delete(0, 'end')
 
 
 def search_btn_func_thread():
@@ -95,7 +108,7 @@ def combobox_click_func():
     if tree.get_children():
         for i in tree.get_children():
             tree.delete(i)
-    single_page(cv)
+    single_page_crawler(cv)
 
 
 def combobox_click_func_thread(event):
@@ -103,7 +116,11 @@ def combobox_click_func_thread(event):
     t2.start()
 
 
-def single_page(n):
+def wrap(string, lenght=50):
+    return '\n'.join(textwrap.wrap(string, lenght))
+
+
+def single_page_crawler(n):
     res = requests.get(
         "https://news.stust.edu.tw/User/RwdNewsList.aspx?page="+str(n), headers=headers)
     soup = BeautifulSoup(res.text, 'lxml')
@@ -112,11 +129,11 @@ def single_page(n):
         hyper = "https://news.stust.edu.tw"+s[2:]
         title = emoji.demojize(item.select('td a')[0].text)
         title_new = re.sub(r':(.*?):', '', title).strip()
-        tree.insert("", "end",  values=(title_new, item.select(
+        tree.insert("", "end",  values=(wrap(title_new), item.select(
             'td.hide-on-small-only.center span')[0].text,  item.select('td.center span')[3].text,  hyper))
 
 
-def search_page(x):
+def search_page_crawler(x):
     n = 1
     res = requests.get("https://news.stust.edu.tw/User/RwdNewsList.aspx?page="+str(n) +
                        "&classid=&DDLItem=%E5%85%AC%E5%91%8A%E6%A8%99%E9%A1%8C&SCont="+str(parse.quote(x.encode('utf8')))+"&SSdate=&SEdate=&dept=", headers=headers)
@@ -126,61 +143,61 @@ def search_page(x):
         hyper = "https://news.stust.edu.tw"+s[2:]
         title = emoji.demojize(item.select('td a')[0].text)
         title_new = re.sub(r':(.*?):', '', title).strip()
-        tree.insert("",  "end",  values=(title_new, item.select(
+        tree.insert("",  "end",  values=(wrap(title_new), item.select(
             'td.hide-on-small-only.center span')[0].text,  item.select('td.center span')[3].text,  hyper))
 
 # GUI SETTINGS
 
 # button
 load_4page_btn = tkinter.Button(text="載入第1頁至第4頁", command=lambda: [
-    load_4page_btn_func_thread()], relief=tkinter.GROOVE)
+    load_4page_btn_func_thread()], relief=tkinter.GROOVE, font=fontset)
 exit_btn = tkinter.Button(text="Click & Quit", command=lambda: [
-    exit_func()], relief=tkinter.GROOVE)
+    exit_func()], relief=tkinter.GROOVE, font=fontset)
 program_info_btn = tkinter.Button(text="應用程式詳情", command=lambda: [
-    program_info_btn_func()], relief=tkinter.GROOVE)
+    program_info_btn_func()], relief=tkinter.GROOVE, font=fontset)
 clear_treeview_btn = tkinter.Button(text="清空列表", command=lambda: [
-    clear_treeview_btn_func()], relief=tkinter.GROOVE)
+    clear_treeview_btn_func()], relief=tkinter.GROOVE, font=fontset)
 search_btn = tkinter.Button(text="搜尋", command=lambda: [
-    search_btn_func_thread()], relief=tkinter.GROOVE)
-    
+    search_btn_func_thread()], relief=tkinter.GROOVE, font=fontset)
 load_4page_btn.place(x=10, y=20)
-exit_btn.place(x=910, y=20)
-program_info_btn.place(x=990, y=20)
-clear_treeview_btn.place(x=850, y=20)
-search_btn.place(x=690, y=20)
+search_btn.place(x=670, y=20)
+clear_treeview_btn.place(x=803, y=20)
+exit_btn.place(x=880, y=20)
+program_info_btn.place(x=980, y=20)
 
 # label
-treeview_guide_label = tkinter.Label(text="點擊兩下開啟列表詳情網頁")
-page_label1 = tkinter.Label(text="單頁顯示第 :")
-page_label2 = tkinter.Label(text="頁")
-search_label = tkinter.Label(text="標題關鍵字 :")
+treeview_guide_label = tkinter.Label(text="點擊兩下開啟列表詳情網頁", font=fontset)
+page_label1 = tkinter.Label(text="單頁顯示第 :", font=fontset)
+page_label2 = tkinter.Label(text="頁", font=fontset)
+search_label = tkinter.Label(text="標題關鍵字 :", font=fontset)
 
-treeview_guide_label.place(x=10, y=70)
-page_label1.place(x=200, y=23)
-page_label2.place(x=360, y=23)
-search_label.place(x=450, y=23)
+treeview_guide_label.place(x=8, y=70)
+page_label1.place(x=180, y=23)
+page_label2.place(x=330, y=23)
+search_label.place(x=430, y=23)
 
 # combobox
-page_combobox = ttk.Combobox(root, state="readonly", width=7)
+page_combobox = ttk.Combobox(root, state="readonly", width=4, font=fontset)
 page_combobox["value"] = ("1", "2", "3", "4", "5", "6", "7", "8", "9", "10")
 page_combobox.current(0)
-page_combobox.place(x=280, y=23)
+page_combobox.place(x=270, y=23)
 page_combobox.bind("<<ComboboxSelected>>", combobox_click_func_thread)
 
-search_entrytext = tkinter.Entry(root, width=20, relief=tkinter.SUNKEN)
-search_entrytext.place(x=530, y=25)
+search_entrytext = tkinter.Entry(
+    root, width=15, relief=tkinter.SUNKEN, font=fontset2)
+search_entrytext.place(x=520, y=25)
 
 # frame
 frame = Frame(root)
-frame.place(x=10, y=100, width=1080, height=580)
+frame.place(x=10, y=100, width=1080, height=680)
 scrollBar = tkinter.Scrollbar(frame)
 scrollBar.pack(side=tkinter.RIGHT, fill=tkinter.Y)
 # treeview
 tree = Treeview(frame, columns=('c1', 'c2', 'c3', 'c4'),
                 show="headings", yscrollcommand=scrollBar.set)
-tree.column('c1', width=860, stretch=False)
-tree.column('c2', width=120, stretch=False)
-tree.column('c3', width=80, stretch=False)
+tree.column('c1', width=825, stretch=False)
+tree.column('c2', width=135, stretch=False)
+tree.column('c3', width=100, stretch=False)
 tree.column('c4', width=0, stretch=False)
 tree.heading('c1', text='標題', anchor='w')
 tree.heading('c2', text='公告單位', anchor='w')
